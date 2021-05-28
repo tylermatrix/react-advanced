07873d772f9184f94c3d8e9fa02099381e26f722
tylermatrix <tylermaatrix@gmail.com>
2021-05-28 14:31:16 -0700

    worked through useEffect and useState basics

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  const [title, setTitle] = useState("title");
+  const handleClick = () => {
+    if(title === "title"){
+      setTitle("new title");
+    } else {
+      setTitle("title");
+    }
+  }
+
+  return <div>
+    <h2>{title}</h2>
+    <button className="btn" onClick={handleClick}>Change Title</button>
+  </div>;
 };
 
 export default ErrorExample;
